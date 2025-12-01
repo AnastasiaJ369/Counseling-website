0c856f2d0e7ea622def79654a4505dcfe237e114
Anastasia <anastasia.m.johnson20@gmail.com>
2025-12-01 15:09:19 -0500

    homepage styling

--- counseling-website/src/pages/homepage.jsx
+++ counseling-website/src/pages/homepage.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import officeImage from '../assets/Modern-Small-Office-Building.jpg';
 
 const Homepage = () => {
   return (
-    <div></div>
+    <div className='min-h-96 max-w-1xl flex flex-col items-center justify-center bg-amber-100 text-black p-4 mt-8 
+    mr-10 ml-10'>
+      <img src={officeImage} alt='Office Building' className='w-100 h-80 object-cover'/>
+      <h1 className='text-2xl font-cursive font-bold mt-3 mb-3'>
+        Welcome to Head and Heart Healing
+      </h1>
+    </div>
   )
 }
 

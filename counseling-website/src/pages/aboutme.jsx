a7783ea8a76b5578e80dcbb52f527a11eadfd30f
Anastasia <anastasia.m.johnson20@gmail.com>
2025-12-04 13:39:17 -0500

    adjust about me placement

--- counseling-website/src/pages/aboutme.jsx
+++ counseling-website/src/pages/aboutme.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
 import headshotImage from '../assets/Headshot.jpeg'
 
+
 const Aboutme = () => {
   return (
     <div className='min-h-96 max-w-1xl flex flex-col items-center justify-center bg-amber-100 text-black p-4 mt-8 
     mr-10 ml-10'>
-      <div className='px-1 w-full'>
-        <img src={headshotImage} alt='Example Headshot' className='flex mt-24 justify-center w-60 aspect-square object-cover rounded-full border-3'/>
-      </div>
-      <div className='float-right px-1 w-full'>
-        <h1 className='text-1xl font-cursive mt-6'/>
-        <p1>
+      <div className='px-1 w-full flex justify-between items-center mx-auto max-w-4xl'>
+        <img src={headshotImage} alt='Example Headshot' className='flex float-left mt-18 mr-20 -ml-20 justify-center w-72 h-72 aspect-square object-cover rounded-full border-3'/>
+        <p1 className= 'text-lg float-right mt-24 justify-center mb-24'>
           Hello! Welcome to Head and Heart Healing! My name is Jane Doe, a licensed clinical social worker (LCSW) with over 10 years of
           experience in providing counseling services! I got my Master's degree in Social Work from the University of Central Florida and
           my ED.S. in Clinical Social Work from Liberty University. I specialize in individual therapy, couples counseling, and family 
